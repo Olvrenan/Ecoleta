3eba3199f80bee327c5154bb9b139be2a50da83f
Renan Oliveira <56043122+Olvrenan@users.noreply.github.com>
2020-06-04 22:29:59 -0300

    first commit - server and web

---
+++ server/src/routes.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
+
+const routes = express.Router();
+const pointsController = new PointsController();
+const itemscontroeller = new ItemsController();
+
+routes.get ('/items', itemscontroeller.index );
+
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
+
+
+export default routes;
